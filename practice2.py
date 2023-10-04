52f90fd5e13779328b84922268735cd54cc3a150
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-10-03 19:58:53 -0500

    Practice 2 completed

---
+++ practice2.py
@@ -0,0 +1,65 @@
+#1
+# i = 1
+# while i < 11:
+#     print(i)
+#     i += 1
+
+#2
+# i = 0
+# while i < 5:
+#     print("hello")
+#     i += 1
+
+#3
+# i = 1
+# while i > 0:
+#     word = input("Enter a word: ")
+#     if word == "stop":
+#         break
+#     i += 1
+
+#4
+# i = 0
+# while i < 105:
+#     print(i)
+#     i += 5
+
+#5
+# i = 0
+# while i < 10:
+#     print(9000)
+#     i += 1
+
+#6
+# i = 1
+# while i > 0:
+#     num = input("Enter a number: ")
+#     if int(num) > 10:
+#         break
+#     i += 1
+
+#7
+# i = 50
+# while i < 71:
+#     print(i)
+#     i += 1
+
+#8
+# i = 0
+# while i < 144:
+#     print("Around the world")
+#     i += 1
+
+# 9
+# i = 1
+# while i > 0:
+#     word = input("Enter a word: ")
+#     if len(word) > 5:
+#         break
+#     i += 1
+
+# 10
+# i = 2
+# while i < 42:
+#     print(i)
+#     i += 2
